4062e985ddbca0a9ce25f361d85cc4e65f77551e
DTZ-2004 <109542751+DTZ-2004@users.noreply.github.com>
2025-12-20 18:17:55 -0500

    Add plot_ssim function for visualizing SSIM data
    Added a new function to plot SSIM from a CSV file. We will use our data and make it into a graph

--- src/visualize.py
+++ src/visualize.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
-
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def show(target, baseline, best):
     plt.figure(figsize=(9, 3))
@@ -27,3 +28,12 @@ def plot(history):
     plt.xlabel("Generation")
     plt.ylabel("SSIM")
     plt.show()
+
+# This is plot ssim # 
+def plot_ssim(csv_path): 
+    df = pd.read_csv(csv_path)
+    plt.plot(df["generation"], df["ssim"])
+    plt.xlabel("Generation")
+    plt.ylabel("SSIM")
+    plt.title("SSIM Over Generations")
+    plt.savefig("figures/ssim_plot.png")
