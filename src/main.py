7ca67080e3427d0a1c677fc7c173a1a4ecf1a4d2
DTZ-2004 <109542751+DTZ-2004@users.noreply.github.com>
2025-12-21 20:03:54 -0500

    Trying to get the compressions to run
    I got it to work in my version, but I am trying to intergrate that into this code

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 from PIL import Image
-from ea_compression import evolve
+from ea_compression import evolve, compress, baseline_reconstruct
+from metrics import evaluate_metrics
 from baseline import baseline_resize
-from visualize import show
+from visualize import show, plot
 
-
+# LOAD IMAGE
+print("Loading image")
 img = Image.open("/Users/aryamac/Desktop/Image-Compression/images/original/Peppers.jpg").convert("RGB")
 original = np.array(img)
 
@@ -13,24 +17,29 @@ baseline = baseline_resize(original)
 
 show(original,baseline, ga)
 
+print("Starting the Bilinear Baseline ")
+    # Baseline Comparison # 
 
-    # Baseline Comparison #
-#
-# # Bilinear baseline reconstruction
-# baseline = baseline_reconstruct(compressed, target.shape)
+# Bilinear baseline reconstruction
+baseline = baseline_reconstruct(compressed, target.shape)
 
-# # Compute metrics
-# b_ssim, b_mse, b_psnr = evaluate_metrics(target, baseline)
-# ea_ssim, ea_mse, ea_psnr = evaluate_metrics(target, best)
-#
-# # Print results
-# print("Bilinear Baseline:")
-# print(f"SSIM: {b_ssim:.4f}, MSE: {b_mse:.2f}, PSNR: {b_psnr:.2f}")
-#
-# print("\nEA Result:")
-# print(f"SSIM: {ea_ssim:.4f}, MSE: {ea_mse:.2f}, PSNR: {ea_psnr:.2f}")
-#
-# # Visualization #
-# show(target, baseline, best)
+# Compute metrics
+b_ssim, b_mse, b_psnr = evaluate_metrics(target, baseline)
+ea_ssim, ea_mse, ea_psnr = evaluate_metrics(target, best)
+
+# Print results
+print("Bilinear Baseline:")
+print(f"SSIM: {b_ssim:.4f}, MSE: {b_mse:.2f}, PSNR: {b_psnr:.2f}")
+
+print("\nEA Result:")
+print(f"SSIM: {ea_ssim:.4f}, MSE: {ea_mse:.2f}, PSNR: {ea_psnr:.2f}")
+
+print("Printing is done")
+
+# Visualization #
+show(target, baseline, best)
+plot(history)
+print("Plot is done")
+
 # plot(history)
 #Lol
